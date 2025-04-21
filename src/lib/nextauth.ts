cd4e07eddcf04a5e3520012b3529bde7ddf5187c
= <mkambouay@gmail.com>
2025-04-21 23:13:34 +0200

    database setup prisma and supabase done + start NextAuth config

---
+++ src/lib/nextauth.ts
@@ -0,0 +1,15 @@
+/*import { NextAuthOptions } from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "./db";
+
+export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
+  callbacks { // facilitan nuestras sesiones y tokens
+
+  },
+  secret: process.env.AUTH_SECRET,
+  adapter: PrismaAdapter(prisma),
+};
+*/
