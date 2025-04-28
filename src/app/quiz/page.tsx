125e5b2715aa16ebcd54baa11dea6915e0113f66
= <mkambouay@gmail.com>
2025-04-28 10:20:31 +0200

    working on form, using zod and react-hook-form

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -1,9 +1,22 @@
+import QuizCreation from "@/components/QuizCreation";
+import { getAuthSession } from "@/lib/nextauth";
+import { redirect } from "next/navigation";
 import React from "react";
 
 type Props = {};
 
-const QuizPage = (props: Props) => {
-  return <div>QuizPage</div>;
+export const metadata = {
+  title: "Quiz | Quizly",
+};
+
+const QuizPage = async (props: Props) => {
+  const session = await getAuthSession();
+
+  if (!session?.user) {
+    return redirect("/");
+  }
+
+  return <QuizCreation />;
 };
 
 export default QuizPage;
